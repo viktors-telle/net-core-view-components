18eb0f15c9cc429d996b2952289cb101c067b250
Viktors Telle <viktors.telle@gmail.com>
2018-10-13 23:58:10 +0300

    Add project files.

---
+++ ViewComponents/Pages/Components/CheckBoxList/CheckBoxListViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewComponents.Pages.Components.RadioButtonList
+{
+    public class CheckBoxListViewComponent : ViewComponent
+    {
+        public CheckBoxListViewComponent() { }
+
+        public IViewComponentResult Invoke(string value)
+        {
+            return View("Default", value);
+        }
+    }
+}
---
+++ ViewComponents/Pages/ConversationalForm.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ViewComponents.Pages
+{
+    public class ConversationalFormModel : PageModel
+    {
+        static Random rnd = new Random();
+        private List<string> _componentNames = new List<string>()
+        {
+            "RadioButtonList",
+            "CheckBoxList"
+        };
+
+
+        public string ViewComponentName { get; set; }
+
+        public Guid Id { get; set; }
+
+        public void OnGet()
+        {
+            ViewComponentName = "RadioButtonList";
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Id = Guid.NewGuid();
+            int r = rnd.Next(_componentNames.Count);
+            ViewComponentName = _componentNames[r];
+            return Page();
+        }
+    }
+}
